6e795e74ede108a84fc5140c0db33cb8cd68f195
Mouad <mouadkhaled2004@gmail.com>
2024-03-02 18:00:20 +0100

    map changes

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -88,7 +88,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'mouadkhaled2004@gmail.com'
 EMAIL_HOST_USER = 'mouadkhaled2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'tdkg lwfw aeex sblj'
+EMAIL_HOST_PASSWORD = 'secret'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
