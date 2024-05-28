990cc3260b1938f12fe2d5e8d26e369ea1691088
Mouad <mouadkhaled2004@gmail.com>
2024-05-28 10:29:01 +0100

    fixed security issues after testing

--- djangoProject3/urls.py
+++ djangoProject3/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('no9-admin/', admin.site.urls),
     path('', include('The_right_DOC.urls')),
 ]
+admin.site.index_title = 'The right DOC'
+admin.site.site_header = 'The right DOC Admin'
