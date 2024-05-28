6f196f13473a5677ebafb388e438413a6d30ca14
Mouad <mouadkhaled2004@gmail.com>
2024-05-28 21:18:47 +0100

    fixed security issues after testing

--- The_right_DOC/static/script.js
+++ The_right_DOC/static/script.js
@@ -21,6 +21,20 @@ btnNavEl.addEventListener("click", function () {
 ////////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
+const observer = new IntersectionObserver((entries) =>{
+        entries.forEach((entry) =>{
+                console.log(entry)
+                if(entry.isIntersecting){
+                  entry.target.classList.add('show');
+                }else{
+                  entry.target.classList.remove('show');
+                }
+        });
+    });
+
+const hidden_el = document.querySelectorAll('.hidden');
+hidden_el.forEach((ele) => observer.observe(ele));
+
 
 ////////////////////////////////////////////////////////////
 // Sticky nav
